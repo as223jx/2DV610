256750c22aa0029a48f18c85e257ec2d5a36a828
as223jx <as223jx@student.lnu.se>
2015-11-23 11:17:31 +0100

    RED

--- ArrayToHTMLTable/src/tests/ArrayToHTMLTests.java
+++ ArrayToHTMLTable/src/tests/ArrayToHTMLTests.java
@@ -152,4 +152,11 @@ public class ArrayToHTMLTests {
 		table.editCell(x, y, "new cell content");
 		assertEquals(table.tableArray[x][y], "new cell content");
 	}
+	
+	@Test
+	public void testSetTableId() {
+		TableMain main = Mockito.mock(TableMain.class);
+		main.setId("10");
+		assertEquals(main.getTableId, "10");
+	}
 }
